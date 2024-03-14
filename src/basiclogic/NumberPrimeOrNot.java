2430ae63c533b801da01635c1985ecaadde1ea01
Ankit Sharma <ankitsharma8102002@gmail.com>
2024-03-14 23:53:27 +0530

    Saving Is PRime code today 14-03-2024

--- src/basiclogic/NumberPrimeOrNot.java
+++ src/basiclogic/NumberPrimeOrNot.java
@@ -1,5 +1,33 @@
 package basiclogic;
 
-public class NumberPrimeOrNot {
+import java.util.Scanner;
 
+public class NumberPrimeOrNot {
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		char choice;
+		do {
+			double num=scan.nextDouble();
+			if(num<=1) {
+				System.out.println("Not prime number.");
+			}
+			if(num==2) {
+				System.out.println("Prime number.");
+			}
+			if(num%2==0) {
+				System.out.println("Not Prime.");
+			}
+			for(int i=3;i<Math.sqrt(num);i+=2) {
+				if(num%i==0) {
+					System.out.println("Not Prime");
+					break;
+				}	
+			}
+			System.out.println("Would you like to continue (Y||y) : ");
+			choice = scan.next().charAt(0);
+		} while (choice == 'Y' || choice == 'y');
+		scan.close();
+	}
 }
